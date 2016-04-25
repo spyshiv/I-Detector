9841a22d69f608922198e5fffb8518f8ae1887f9
spyshiv <spyshiv@gmail.com>
2016-04-26 02:10:28 +0530

    Add image model

--- Id_backend/facedetection/models.py
+++ Id_backend/facedetection/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Imagedata(models.Model):
-	model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+	upload_path = 'media/faces'
+	model_pic = models.ImageField(upload_to = upload_path, default = 'pic_folder/None/no-img.jpg')
