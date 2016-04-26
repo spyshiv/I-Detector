166bf1a53b386cf438408a7dfda4bb89c3743cfc
spyshiv <spyshiv@gmail.com>
2016-04-26 05:48:16 +0530

    display all uploaded image

--- Id_backend/facedetection/models.py
+++ Id_backend/facedetection/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Imagedata(models.Model):
-	upload_path = 'media/faces'
-	model_pic = models.ImageField(upload_to = upload_path, default = 'pic_folder/None/no-img.jpg')
+	upload_path = 'static/images/upload/'
+	model_pic = models.ImageField(upload_to = upload_path, default = 'static/None/no-img.jpg')
